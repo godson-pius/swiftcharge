c2cbfd766ef024c4cefc1800e6742259eccd8857
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2025-07-08 12:49:46 +0100

    removed createToken function

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 
-export const createToken = (payload: object, expiresIn: string = "1h"): string => {
-    return jwt.sign(payload, process.env.JWT_SECRET as string, {
-        expiresIn,
-    });
-}
+// export const createToken = (payload: object, expiresIn: string = "1h"): string => {
+//     return jwt.sign(payload, process.env.JWT_SECRET as string, {
+//         expiresIn,
+//     });
+// }
 
 export const verifyToken = (token: string): string | jwt.JwtPayload => {
     return jwt.verify(token, process.env.JWT_SECRET as string);
